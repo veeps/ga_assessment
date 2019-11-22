497f4429541a7b09051b721f971724e480c4ffbe
Vivian Peng <vivianpeng@vivians-mbp.home>
2019-11-21 20:15:05 -0800

    finished problem set

--- kickstarter/ga_assessment.R
+++ kickstarter/ga_assessment.R
@@ -8,13 +8,23 @@ library(useful)
 library(coefplot)
 library(magrittr)
 library(forcats)
+library(lubridate)
+library(margins)
 
 kickstarter <- read_csv("data/DSI_kickstarterscrape_dataset.csv",col_names =  TRUE)
 kickstarter <- kickstarter %>%
   janitor::clean_names()
 
-colnames(kickstarter)
+# combine categories for Film & Video
+kickstarter$category <- str_replace_all(kickstarter$category, "Film &amp; Video", "Film & Video")
 
+
+# conver date to date class
+kickstarter <- separate(data = kickstarter, col = funded_date, into = c("Day_of_Week", "Date"), sep = "\\,")
+kickstarter$Date <- dmy_hms(kickstarter$Date)
+
+kickstarter %<>%
+  mutate_at(vars(Date), funs(year, month, day, hour))
 
 # What is the mean (total) pledge that projects get? (not per backer) *
 
@@ -64,14 +74,96 @@ kickstarter_completed <- kickstarter %>%
   filter(status != "live") %>%
   mutate(status_binary = ifelse(status== "successful", 1, 0 ))
 
-cor(kickstarter_completed$duration, kickstarter_completed$status_binary) 
+kickstarter_completed %<>%
+  mutate(category_factor = fct_lump(kickstarter_completed$category,6))
+
+############ 
+############ split data set
+set.seed(42)
+rows <- sample(nrow(kickstarter_completed))
+kickstarter_completed <- kickstarter_completed[rows, ]
+split <- round(nrow(kickstarter_completed) * .80)
+
+train <- kickstarter_completed[1:split, ]
+test <- kickstarter_completed[(split + 1):nrow(kickstarter_completed), ]
 
 
-logistic_regression <- glm(status_binary ~ duration + goal, 
+
+
+############ 
+############ 
+############ BUILDING MODELS
+model_1 <- glm(status_binary ~ duration, family =binomial(link="logit"), data = train)
+model_2 <- glm(status_binary ~ goal, family = binomial(link="logit"), data = train )
+model_3 <- glm(status_binary~category_factor, family =binomial(link="logit"), data = train)
+model_4 <- glm(status_binary~month, family =binomial(link="logit"), data = train)
+model_5 <- glm(status_binary~Day_of_Week, family =binomial(link="logit"), data = train)
+model_6 <- glm(status_binary~Day_of_Week + month, family =binomial(link="logit"), data = train)
+model_7 <- glm(status_binary~hour, family =binomial(link="logit"), data = train)
+
+
+
+###### predict
+p1 <- predict(model_1, test, type = "response")
+test$predicted_success_duration <- p1
+ggplot(test, aes(x = duration, y = predicted_success_duration)) +
+  geom_point()
+
+
+# What's the ideal pledge goal?
+# let's look at the average pledge goal for successful projects
+kickstarter_successful <- kickstarter %>%
+  filter(status == "successful")
+avg_goal_successful <- mean(kickstarter_successful$goal) 
+
+
+p2 <- predict(model_2, test, type = "response")
+test$predicted_success_goal <- p2
+ggplot(test, aes(x = goal, y = predicted_success_goal)) +
+  geom_point()
+
+# What type of projects would be most successful at getting funded?
+# Combine the two Film & video categories - account for special characters
+
+p3 <- predict(model_3, test, type = "response")
+test$predicted_success_category <- p3
+
+
+ggplot(test, aes(x = category_factor, y = predicted_success_category)) +
+  geom_point()
+
+# Is there an ideal month/day/time to launch a campaign?
+p4 <- predict(model_4, test, type = "response")
+test$predicted_success_month <- p4
+
+ggplot(test, aes(x = month, y = predicted_success_month)) +
+  geom_point()
+
+
+
+p5 <- predict(model_5, test, type = "response")
+test$predicted_success_day <- p5
+
+ggplot(test, aes(x = Day_of_Week, y = predicted_success_day)) +
+  geom_point()
+
+
+cplot(model_7, "hour", what = "prediction")
+
+
+
+
+
+
+
+######################### exploratory
+logistic_regression <- glm(status_binary ~ duration + goal , 
                            family =binomial(link="logit"), 
                            data = kickstarter_completed) 
 
-margins(logistic_regression, variables = "duration", type = "response")
+is.na(kickstarter_completed$goal)
+
+margins(model_2, variables = "goal", type = "response")
 
 cplot(logistic_regression, "duration", what = "prediction")
 
@@ -79,61 +171,6 @@ cplot(logistic_regression, "duration", what = "prediction")
 exp(logistic_regression$coefficients)
 
 coefplot(logistic_regression)
-
-## split data set
-set.seed(42)
-rows <- sample(nrow(kickstarter_completed))
-kickstarter_completed <- kickstarter_completed[rows, ]
-split <- round(nrow(kickstarter_completed) * .80)
-
-train <- kickstarter_completed[1:split, ]
-test <- kickstarter_completed[(split + 1):nrow(kickstarter_completed), ]
-
-model_1 <- glm(status_binary ~ duration, family =binomial(link="logit"), data = train)
-p <- predict(model_1, test, type = "response")
-test$predicted_success <- p
-
-head(p)
-# calculate errors
-error <- p - test[["status_binary"]]
-sqrt(mean(error^2))
-
-
-ggplot(test, aes(x = duration, y = predicted_success)) +
-  geom_point()
-
-
-# What's the ideal pledge goal?
-# let's look at the average pledge goal for successful projects
-avg_goal_successful <- mean(kickstarter_successful$goal) 
-
-
-# What type of projects would be most successful at getting funded?
-# Combine the two Film & video categories - account for special characters
-unique(kickstarter_completed$category)
-kickstarter_completed$category <- str_replace_all(kickstarter_completed$category, "Film &amp; Video", "Film & Video")
-
-kickstarter_completed %<>%
-  mutate(category_factor = fct_lump(kickstarter_completed$category,6))
-
-
-set.seed(42)
-rows <- sample(nrow(kickstarter_completed))
-kickstarter_completed <- kickstarter_completed[rows, ]
-split <- round(nrow(kickstarter_completed) * .80)
-
-train <- kickstarter_completed[1:split, ]
-test <- kickstarter_completed[(split + 1):nrow(kickstarter_completed), ]
-
-model_2 <- glm(status_binary~category_factor, family =binomial(link="logit"), data = train)
-p2 <- predict(model_2, test, type = "response")
-test$predicted_success_category <- p2
-
-
-ggplot(test, aes(x = category, y = predicted_success_category)) +
-  geom_point()
-
-# Is there an ideal month/day/time to launch a campaign?
 
 
 
